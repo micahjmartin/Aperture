9c4f9913bf4fdf8694762a62d19d09f86633b003
micahjmartin <mjm5097@rit.edu>
2020-10-02 00:29:24 -0400

    renamed repos

--- assets/js/searchable.js
+++ assets/js/searchable.js
@@ -1,5 +1,5 @@
 ---
 ---
 const documents = [
-{% for repo in site.configs %}{"id": "{{ repo.name | slugify }}", "topics": "{{ repo.topics | join: " " }}", "language": "{{ repo.language }}", "description": `{{ repo.description  }}`, "writeup": `{{ repo.writeup }}`, "readme": `{{ repo.readme | escape }}`},
+{% for repo in site.repos %}{"id": "{{ repo.name | slugify }}", "topics": "{{ repo.topics | join: " " }}", "language": "{{ repo.language }}", "description": `{{ repo.description  }}`, "writeup": `{{ repo.writeup }}`, "readme": `{{ repo.readme | escape }}`},
 {% endfor %}]
